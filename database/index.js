76c0406c53f02498fcc2bb6a71e53ca179509e1f
Ali yoho <studycatcat@yahoo.com>
2022-03-22 11:38:19 -0700

    added schema

--- database/index.js
+++ database/index.js
@@ -3,14 +3,20 @@ mongoose.connect('mongodb://localhost/fetcher');
 
 let repoSchema = mongoose.Schema({
   // TODO: your schema here!
+  id: Number,
+  repoName: String,
+  ownerName: String,
+  repoLink: String,
 });
 
+//compiling schema into a Model:
 let Repo = mongoose.model('Repo', repoSchema);
 
 let save = (/* TODO */) => {
   // TODO: Your code here
   // This function should save a repo or repos to
   // the MongoDB
+
 }
 
 module.exports.save = save;
